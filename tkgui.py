87fecec12a42ad3505091b797086460bff400681
user <user@localhost.localdomain>
2020-07-29 18:37:15 +0300

    Bad day

--- tkgui.py
+++ tkgui.py
@@ -1,8 +1,11 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
 from tkinter import *
 import tkinter as tk
 import sys
 import fileinput
 import glob
+from iosapa import IoSapa
 
 root = Tk()
 root.title('SaPa')
@@ -35,43 +38,46 @@ class Log:
         nw.title("SaPa Control Accounts")
         nw.geometry("600x600")
 
-        #Three Entrys
+        #Three Entrys with labels
         self.acLabel=Label(nw,text="Παροχως Λογαριασμου").grid(row=0,column=3)
-        self.eAcc = Entry(nw).grid(row=1,column=3)
+        self.eAcc = Entry(nw)
+        self.eAcc.grid(row=1,column=3)
 
         self.usLabel=Label(nw,text="Ονομα Λογαριασμου").grid(row=2,column=3)
-        self.eUs = Entry(nw).grid(row=3,column=3)
+        self.eUs = Entry(nw)
+        self.eUs.grid(row=3,column=3)
 
 
         self.paLabel=Label(nw,text="Κωδικος Λογαριασμου").grid(row=4,column=3)
-        self.ePa = Entry(nw).grid(row=5,column=3)
+        self.ePa = Entry(nw)
+        self.ePa.grid(row=5,column=3)
 
 
         self.btnInsert=Button(nw,text="Επιβεβεωση",bg="green",command=self.insertF).grid(row=6,column=3)
 
         self.btnRemove=Button(nw,text="Αφαιρεση",bg="red",command=self.removeF).grid(row=8,column=0)
 
-        #Text
+        #Create TextArrea
         self.text = Text(nw,width=45,height=25,padx=10)
         self.text.grid(row=0,column=0,columnspan=3,rowspan=6)
         x=0
         self.filename = glob.glob("./data/*.txt")
-        #print(filename)
+     
+        #open file for read in text area
         for i in self.filename:
             with open(str(self.filename[x]),"r+") as fo:
-           #fo = open(str(filename[x]),"r+")
                 for line in fo:
                     self.text.insert(END,line)
-                    
                 self.text.insert(END,"\n--------------------------------------------\n")
                 x+=1
         self.text.config(state=DISABLED)
 
     def insertF(self):
-        pass
+        iosapa=IoSapa()
+        iosapa.writeF(str(self.eAcc.get()),str(self.eUs.get()),str(self.ePa.get()))
     def removeF(self):
-        pass
-
+        iosapa=IoSapa()
+        iosapa.removeF(str(self.eAcc.get()))
 
 def main():
     o1=Log(root)  
