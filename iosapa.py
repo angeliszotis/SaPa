1c4dfec3e9885c8851e2d9451317eaed9595d8aa
user <user@localhost.localdomain>
2020-07-29 18:44:49 +0300

    bad day yoyo

--- iosapa.py
+++ iosapa.py
@@ -4,7 +4,6 @@ import Acc
 import fileinput
 import glob
 import os
-from array import *
 
 class IoSapa:
     def __init__(self):
@@ -15,20 +14,6 @@ class IoSapa:
             account = Acc.Acc(filename,username,password)
             fo.write(account.toString())
 
-    def readF(self):
-        x=0
-        y=0
-        filename = glob.glob("data/*.txt")
-        arrprint = []
-        for i in filename:
-            with open(str(filename[x]),"r+") as fo:
-                x+=1
-                for line in fo:
-                    print (line) 
-                    arrprint.insert(x,[x,line])
-                    y+=1
-        return arrprint
-
     def removeF(self,path):
 
         print (path)
@@ -36,6 +21,3 @@ class IoSapa:
             os.remove('./data/{}.txt'.format(path))
         else:
             print("Δεν υπαρχει ο παροχος");
-
-p1 = IoSapa()
-p1.readF()
